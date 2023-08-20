4bd5badd7510494f608b820525d88874ee97a844
mofe <ogunbiyioladapo33@gmail.com>
2023-08-20 09:13:59 +0100

    golang impl

--- golang/main.go
+++ golang/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"mofe64/dsa/solutions"
+)
 
 func main() {
 	fmt.Println("hello world")
+	nums := []int{1, 12, -5, -6, 50, 3}
+	max := solutions.MaxAvgOfSubArraySlidingWindowSolution(nums, 4)
+	fmt.Println(max)
+	nums = []int{0, 1, 1, 3, 3}
+	max = solutions.MaxAvgOfSubArraySlidingWindowSolution(nums, 4)
+	fmt.Println(max)
+
 }
